d3c395ce79cb3f6e4042c5dfbb2b73d97d2816b4
natively.dev <api@natively.dev>
2026-01-30 20:33:48 +0000

    fix: Update Supabase ANON KEY for extended validity

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://brgmdozyasjreofznlkf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyZ21kb3p5YXNqcmVvZnpubGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2MDcyOTMsImV4cCI6MjA2NDE4MzI5M30.QGdIwHshzHAL7HpnBLckLfgFqCYnRpnLkCmuk_qLqJg"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyZ21kb3p5YXNqcmVvZnpubGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NTg2MzgsImV4cCI6MjA4NTMzNDYzOH0.Jo3xb-qD7d9nqeptMKCzVh86mUv-rkGtegOIpGb5LIw"
 );
